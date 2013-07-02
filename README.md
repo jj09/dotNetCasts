a23096e7ccf9ed28bf4527e33835d739df8637c3
jjedrys <jjedrys@santoslab.org>
2013-07-01 21:24:16 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotNetCasts
+
+This is the simple vlog application for publishing podcasts.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DotNetCasts::Application.config.secret_token = '5d566d0630f063bec1dd618819ae2a9fcd62fa38af9225bf2db1ebe4051d093dbe870dc4011537c74992441062a1b8620b90a5a00a0533ade4a183123e48cf4f'
