6510a026b1a02e4f87e1257c57a070861727190f
jjedrys <jjedrys@santoslab.org>
2013-07-06 00:48:38 -0500

    Fix migration tags_vodcasts

--- db/migrate/20130706025324_create_tags_vodcasts_join.rb
+++ db/migrate/20130706025324_create_tags_vodcasts_join.rb
@@ -1,11 +1,11 @@
 class CreateTagsVodcastsJoin < ActiveRecord::Migration
   def self.up
-    create_table 'vodcasts_tags', :id => false do |t|
+    create_table 'tags_vodcasts', :id => false do |t|
       t.column 'vodcast_id', :integer
       t.column 'tag_id', :integer
      end
    end
  def self.down
-   drop_table 'vodcasts_tags'
+   drop_table 'tags_vodcasts'
  end
 end
